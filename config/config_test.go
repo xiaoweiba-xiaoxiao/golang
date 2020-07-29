6154a82de676c2045546b5a05e2095cb6a498fad
xiaoweiba <xiaochaoren80@gmail.com>
2020-07-29 17:29:57 +0800

    config

--- config/config_test.go
+++ config/config_test.go
@@ -1,22 +1,38 @@
 package config
 
 import (
-	"io/ioutil"
+	_ "fmt"
 	"testing"
 )
 
-func TestConfig(t *testing.T) {
-	configFile := "config.ini"
-	data, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		t.Error("read file failed")
-		return
+// func TestConfig(t *testing.T) {
+// 	configFile := "config.ini"
+// 	data, err := ioutil.ReadFile(configFile)
+// 	if err != nil {
+// 		t.Error("read file failed")
+// 		return
+// 	}
+// 	var result *ConfigInfo = &ConfigInfo{}
+// 	//var testint *int
+// 	err = UnMarshal(data, result)
+// 	if err != nil {
+// 		t.Error("unmashal filed Error:", err)
+// 	}
+// 	t.Logf("unmashal success,conf: %#v", result)
+// }
+
+func TestWriteConfig(t *testing.T) {
+	data := ConfigInfo{
+		Serverconf: ServerConfig{Ip: "192.168.234.200", Port: 100},
+		DBconfig:   DBConfig{Host: "192.168.234.200", Port: 3306, UserName: "root", Passwd: "root", DataBase: "test"},
 	}
-	var result *ConfigInfo = &ConfigInfo{}
-	//var testint *int
-	err = UnMarshal(data, result)
+	result, err := Marshal(data)
 	if err != nil {
-		t.Error("unmashal filed Error:", err)
+		t.Error("mashal failed Error:", err)
 	}
-	t.Logf("unmashal success,conf: %#v", result)
+	err = WriteFile("/home/golang/conf/test.conf", result)
+	if err != nil {
+		t.Errorf("write file faild %s", err)
+	}
+	t.Logf("write success")
 }
--- config/iniconfig.go
+++ config/iniconfig.go
@@ -1,10 +1,11 @@
 package config
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
 	"reflect"
-	_ "reflect"
 	"strconv"
 	"strings"
 )
@@ -34,28 +35,87 @@ func Marshal(data interface{}) (result []byte, err error) {
 	// 	switch k {
 	// 		case
 	// 	}
+	//获取传入的数据类型
+	t := reflect.TypeOf(data)
+	if t.Kind() != reflect.Struct {
+		err = errors.New("plese pass Struct")
+		return
+	}
+	//获取传入的值
+	valinfo := reflect.ValueOf(data)
+	//用于存储需要写入文件的数据
+	var wrtdata []string
+	//获取节点字节的信息
+	for i := 0; i < t.NumField(); i++ {
+		sectionField := t.Field(i)      //获取data的section信息
+		sectinVal := valinfo.Field(i)   //获取section的值
+		childField := sectionField.Type //获取section的类型 如果不是结构体就跳过
+		if childField.Kind() != reflect.Struct {
+			continue
+		}
+		//获取section的tag值
+		tagVal := sectionField.Tag.Get("ini")
+		if len(tagVal) == 0 {
+			tagVal = sectionField.Name
+		}
+		//格式话section的节点名称
+		section := fmt.Sprintf("[%s]\n", tagVal)
+		wrtdata = append(wrtdata, section)
+		//遍历每个节点的field信息
+		for j := 0; j < childField.NumField(); j++ {
+			childFieldType := childField.Field(j) //获取每个field的type
+			childFieldVal := sectinVal.Field(j)   //获取每个field的val信息
+			key := childFieldType.Tag.Get("ini")  // 获取每个field的tag信息
+
+			if len(key) == 0 {
+				key = childFieldType.Name
+			}
+			//格式化field信息添加到wrtdata里面
+			fieldStr := fmt.Sprintf("%s=%v\n", key, childFieldVal)
+			wrtdata = append(wrtdata, fieldStr)
+		}
+		wrtdata = append(wrtdata, "\n")
+	}
+	for _, val := range wrtdata {
+		byteVal := []byte(val)
+		result = append(result, byteVal...)
+	}
+	return
+}
+
+func WriteFile(file string, data []byte) (err error) {
+	f, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	newWrite := bufio.NewWriter(f)
+	newWrite.Write(data)
+	newWrite.Flush()
 	return
 }
 
 func UnMarshal(data []byte, result interface{}) (err error) {
 	lineArr := strings.Split(string(data), "\n")
-	_ = lineArr
 	typeInfo := reflect.TypeOf(result)
 	if typeInfo.Kind() != reflect.Ptr {
 		err = errors.New("plese pass pointer")
 		return
 	}
+	//获取指针位置的类型
 	typeStruct := typeInfo.Elem()
 	if typeStruct.Kind() != reflect.Struct {
 		err = errors.New("plese pass struct")
 		return
 	}
+	//存取节点字段
 	var lastFieldName string
 	for index, line := range lineArr {
 		line = strings.TrimSpace(line)
 		if len(line) == 0 || line[0] == '#' || line[0] == ';' {
 			continue
 		}
+		//解析节点
 		if line[0] == '[' {
 			lastFieldName, err = parseSection(line, index, typeStruct)
 			if err != nil {
@@ -64,6 +124,7 @@ func UnMarshal(data []byte, result interface{}) (err error) {
 			fmt.Println(lastFieldName)
 			continue
 		}
+		//解析节点所在的iteam
 		err = parseItem(lastFieldName, line, result)
 		if err != nil {
 			err = fmt.Errorf("%s line:%d", err, index+1)
@@ -73,17 +134,22 @@ func UnMarshal(data []byte, result interface{}) (err error) {
 	return
 }
 
+//解析节点
 func parseSection(line string, index int, typeStruct reflect.Type) (fieldName string, err error) {
+	//节点非法输出,空括号
 	if line[0] == '[' && len(line) <= 2 {
 		err = fmt.Errorf("syntax error,invalid section:%s  line:%d", line, index+1)
 		return
 	}
+	//节点非法情况2 "[" 没有 "]"
 	if line[0] == '[' && line[len(line)-1] != ']' {
 		err = fmt.Errorf("syntax error,invalid section:%s  line:%d", line, index+1)
 		return
 	}
+	//左右都是括号的情况
 	if line[0] == '[' && line[len(line)-1] == ']' {
 		section := strings.TrimSpace(line[1 : len(line)-1])
+		//左右括号中间为空的情况
 		if len(section) == 0 {
 			err = fmt.Errorf("syntax error,invalid section:%s  line:%d", line, index+1)
 			return
@@ -100,40 +166,55 @@ func parseSection(line string, index int, typeStruct reflect.Type) (fieldName st
 	return
 }
 
+//解析节点
 func parseItem(lastFieldName string, line string, result interface{}) (err error) {
+	//获取等号的位置
 	equlindex := strings.Index(line, "=")
+	//如果没有等号
 	if equlindex == -1 {
 		err = fmt.Errorf("syntax error,invalid section:%s", line)
 		return
 	}
+	//获取等号左边的值为key,右边的值为val，并且去掉左右空格
 	key := strings.TrimSpace(line[:equlindex])
 	val := strings.TrimSpace(line[equlindex+1:])
+	//如果key的长度为0，则key不存在
 	if len(key) == 0 {
 		err = fmt.Errorf("syntax error,invalid section:%s", line)
 		return
 	}
+	//获取result的类型
 	resultVal := reflect.ValueOf(result)
+	//获取地址的filedname的值,值为一个结构体类型
 	sectionVal := resultVal.Elem().FieldByName(lastFieldName)
+	//获取值得类型
 	sectionType := sectionVal.Type()
+	//如果值得类型不是结构体 返回错误
 	if sectionType.Kind() != reflect.Struct {
 		err = fmt.Errorf("field must be struct:%s", line)
 		return
 	}
+	//设置字段名
 	keyFieldName := ""
 	for i := 0; i < sectionType.NumField(); i++ {
 		field := sectionType.Field(i)
+		//如果tag名称和key相同，将字段名赋值为当前的字段名
 		if field.Tag.Get("ini") == key {
 			keyFieldName = field.Name
 			break
 		}
 	}
+	//如果字段名字为空不处理则返回
 	if len(keyFieldName) == 0 {
 		return
 	}
+	//获取
 	fieldval := sectionVal.FieldByName(keyFieldName)
+	//如果获取不到值就返回错误
 	if fieldval == reflect.ValueOf(nil) {
 		return
 	}
+	// 根据字段值的类型来设置字段的值
 	switch fieldval.Type().Kind() {
 	case reflect.String:
 		fieldval.SetString(val)
